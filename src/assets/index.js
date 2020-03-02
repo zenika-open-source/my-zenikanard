56a962473a53d402852af4312165c42e309b9e7d
Benjamin Petetot <bpetetot@gmail.com>
2020-03-02 22:56:10 +0100

    :sparkles: add shapes

--- src/assets/index.js
+++ src/assets/index.js
@@ -10,10 +10,14 @@ import {ReactComponent as Eye2} from './shapes/eye-2.svg';
 import {ReactComponent as IconEye2} from './icons/eye-2.svg';
 import {ReactComponent as Eye3} from './shapes/eye-3.svg';
 import {ReactComponent as IconEye3} from './icons/eye-3.svg';
+import {ReactComponent as Eye4} from './shapes/eye-4.svg';
+import {ReactComponent as IconEye4} from './icons/eye-4.svg';
 import {ReactComponent as Face1} from './shapes/face-1.svg';
 import {ReactComponent as IconFace1} from './icons/face-1.svg';
 import {ReactComponent as Face2} from './shapes/face-2.svg';
 import {ReactComponent as IconFace2} from './icons/face-2.svg';
+import {ReactComponent as Face3} from './shapes/face-3.svg';
+import {ReactComponent as IconFace3} from './icons/face-3.svg';
 import {ReactComponent as Floor} from './shapes/floor.svg';
 import {ReactComponent as IconFloor} from './icons/floor.svg';
 import {ReactComponent as Glasses1} from './shapes/glasses-1.svg';
@@ -22,12 +26,34 @@ import {ReactComponent as Glasses2} from './shapes/glasses-2.svg';
 import {ReactComponent as IconGlasses2} from './icons/glasses-2.svg';
 import {ReactComponent as Glasses3} from './shapes/glasses-3.svg';
 import {ReactComponent as IconGlasses3} from './icons/glasses-3.svg';
+import {ReactComponent as Glasses4} from './shapes/glasses-4.svg';
+import {ReactComponent as IconGlasses4} from './icons/glasses-4.svg';
+import {ReactComponent as Glasses5} from './shapes/glasses-5.svg';
+import {ReactComponent as IconGlasses5} from './icons/glasses-5.svg';
+import {ReactComponent as Glasses6} from './shapes/glasses-6.svg';
+import {ReactComponent as IconGlasses6} from './icons/glasses-6.svg';
 import {ReactComponent as Hair1} from './shapes/hair-1.svg';
 import {ReactComponent as IconHair1} from './icons/hair-1.svg';
 import {ReactComponent as Hair2} from './shapes/hair-2.svg';
 import {ReactComponent as IconHair2} from './icons/hair-2.svg';
+import {ReactComponent as Hait3} from './shapes/hait-3.svg';
+import {ReactComponent as IconHait3} from './icons/hait-3.svg';
 import {ReactComponent as Hat1} from './shapes/hat-1.svg';
 import {ReactComponent as IconHat1} from './icons/hat-1.svg';
+import {ReactComponent as Hat2} from './shapes/hat-2.svg';
+import {ReactComponent as IconHat2} from './icons/hat-2.svg';
+import {ReactComponent as Hat3} from './shapes/hat-3.svg';
+import {ReactComponent as IconHat3} from './icons/hat-3.svg';
+import {ReactComponent as Hat4} from './shapes/hat-4.svg';
+import {ReactComponent as IconHat4} from './icons/hat-4.svg';
+import {ReactComponent as Hat5} from './shapes/hat-5.svg';
+import {ReactComponent as IconHat5} from './icons/hat-5.svg';
+import {ReactComponent as Hat6} from './shapes/hat-6.svg';
+import {ReactComponent as IconHat6} from './icons/hat-6.svg';
+import {ReactComponent as Hat7} from './shapes/hat-7.svg';
+import {ReactComponent as IconHat7} from './icons/hat-7.svg';
+import {ReactComponent as Hat8} from './shapes/hat-8.svg';
+import {ReactComponent as IconHat8} from './icons/hat-8.svg';
 import {ReactComponent as Head} from './shapes/head.svg';
 import {ReactComponent as IconHead} from './icons/head.svg';
 import {ReactComponent as Mouth1} from './shapes/mouth-1.svg';
@@ -73,6 +99,11 @@ export default [{
     icon: IconEye3,
   },
 {
+    name: 'eye-4',
+    asset: Eye4,
+    icon: IconEye4,
+  },
+{
     name: 'face-1',
     asset: Face1,
     icon: IconFace1,
@@ -81,6 +112,11 @@ export default [{
     name: 'face-2',
     asset: Face2,
     icon: IconFace2,
+  },
+{
+    name: 'face-3',
+    asset: Face3,
+    icon: IconFace3,
   },
 {
     name: 'floor',
@@ -103,6 +139,21 @@ export default [{
     icon: IconGlasses3,
   },
 {
+    name: 'glasses-4',
+    asset: Glasses4,
+    icon: IconGlasses4,
+  },
+{
+    name: 'glasses-5',
+    asset: Glasses5,
+    icon: IconGlasses5,
+  },
+{
+    name: 'glasses-6',
+    asset: Glasses6,
+    icon: IconGlasses6,
+  },
+{
     name: 'hair-1',
     asset: Hair1,
     icon: IconHair1,
@@ -113,9 +164,49 @@ export default [{
     icon: IconHair2,
   },
 {
+    name: 'hait-3',
+    asset: Hait3,
+    icon: IconHait3,
+  },
+{
     name: 'hat-1',
     asset: Hat1,
     icon: IconHat1,
+  },
+{
+    name: 'hat-2',
+    asset: Hat2,
+    icon: IconHat2,
+  },
+{
+    name: 'hat-3',
+    asset: Hat3,
+    icon: IconHat3,
+  },
+{
+    name: 'hat-4',
+    asset: Hat4,
+    icon: IconHat4,
+  },
+{
+    name: 'hat-5',
+    asset: Hat5,
+    icon: IconHat5,
+  },
+{
+    name: 'hat-6',
+    asset: Hat6,
+    icon: IconHat6,
+  },
+{
+    name: 'hat-7',
+    asset: Hat7,
+    icon: IconHat7,
+  },
+{
+    name: 'hat-8',
+    asset: Hat8,
+    icon: IconHat8,
   },
 {
     name: 'head',
