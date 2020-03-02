2153a2bfba78c7d140a707dd9cf5615f105ece09
Benjamin Petetot <bpetetot@gmail.com>
2020-03-02 11:09:58 +0100

    :sparkles: download to png

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import cn from 'classnames';
+import svgExport from 'save-svg-as-png';
 
 import {ReactComponent as Body} from './assets/shapes/body.svg';
 import {ReactComponent as Head} from './assets/shapes/head.svg';
 import {ReactComponent as Floor} from './assets/shapes/floor.svg';
 import {ReactComponent as Random} from './icons/random.svg';
 import {ReactComponent as Trash} from './icons/trash.svg';
+import {ReactComponent as Download} from './icons/download.svg';
 import {ReactComponent as ByZenika} from './icons/byzenika.svg';
 
 
@@ -20,6 +22,7 @@ function getRandomInt(max: number) {
 }
 
 function App() {
+  const svgElement = useRef<SVGSVGElement>(null);
   const [assets, setAssets] = useState<SelectedAssets>(DEFAULT_ASSETS);
   const [category, setCategory] = useState<number>(0);
 
@@ -59,6 +62,8 @@ function App() {
 
   const reset = () => setAssets(DEFAULT_ASSETS);
 
+  const download = () => svgExport.saveSvgAsPng(svgElement?.current, "zenikanard.png")
+
   return (
     <div className={styles.app}>
       <div className={styles.background} />
@@ -70,7 +75,7 @@ function App() {
       </div>
       <div className={styles.main}>
         <div className={styles.canvas}>
-          <svg viewBox="0 0 2000 2000">
+          <svg ref={svgElement} width="2000" height="2000" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2000 2000">
             <Floor />
             <Body />
             {getLayerAsset('body-tatoo')}
@@ -116,6 +121,9 @@ function App() {
           </button>
           <button className={styles.circle} onClick={randomize}>
             <Random height="24px" width="24px" />
+          </button>
+          <button className={styles.circle} onClick={download}>
+            <Download height="24px" width="24px" />
           </button>
         </div>
       </div>
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,3 @@
 /// <reference types="react-scripts" />
+
+declare module 'save-svg-as-png';
