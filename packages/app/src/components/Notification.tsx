0b2940dda1baf8e1cb08d2d228f22ae327a76bc4
Benjamin Petetot <bpetetot@gmail.com>
2020-04-08 14:50:15 +0200

    :sparkles: add notification to update app

---
+++ packages/app/src/components/Notification.tsx
@@ -0,0 +1,40 @@
+import React, { FC, useState } from 'react'
+import cn from 'classnames'
+
+import styles from './Notification.module.css'
+
+type NotificationProps = {
+  actionLabel?: string
+  onActionClick?: () => void
+}
+
+const Notification: FC<NotificationProps> = ({
+  children,
+  actionLabel = 'Close',
+  onActionClick,
+}) => {
+  const [open, setOpen] = useState(true)
+
+  const handleAction = () => {
+    if (onActionClick) onActionClick()
+    setOpen(false)
+  }
+
+  if (!open) return null
+
+  return (
+    <div className={styles.notification}>
+      <p>{children}</p>
+      <button
+        type="button"
+        onClick={handleAction}
+        className={cn(styles.button, styles.primary)}
+        aria-label={actionLabel}
+      >
+        {actionLabel}
+      </button>
+    </div>
+  )
+}
+
+export default Notification
